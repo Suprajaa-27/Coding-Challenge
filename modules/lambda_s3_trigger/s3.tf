af20af4a67aef2dbab0d0bc0335134fc8bd78b59
suprajaa-27 <suprajaa96@gmail.com>
2023-12-07 17:04:02 +0100

    Final revision of readme.md and s3

--- modules/lambda_s3_trigger/s3.tf
+++ modules/lambda_s3_trigger/s3.tf
@@ -1,9 +1,10 @@
 # Creating s3 bucket
-# Creating s3 bucket
 resource "aws_s3_bucket" "s3_bucket" {
+  
   bucket = var.bucket_name
 }
 resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
     id = "object-lifecycle-rule"
@@ -24,9 +25,22 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "bucket-versioning" {
+resource "aws_s3_bucket_versioning" "object-versioning" {
+  
   bucket = aws_s3_bucket.s3_bucket.id
   versioning_configuration {
     status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_object_lock_configuration" "object-lock" {
+  
+  bucket = aws_s3_bucket.s3_bucket.id
+  object_lock_enabled = "Enabled"
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
   }
 }
