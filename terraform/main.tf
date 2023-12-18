6ecb9eeb12569ce5110a5d81f545f72007587e67
suprajaa-27 <suprajaa96@gmail.com>
2023-12-18 16:13:54 +0100

    updated s3.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,40 +1,40 @@
-# # Resource to create a lambda function
+# Resource to create a lambda function
 
-# resource "aws_lambda_function" "s3_trigger_lambda" {
-#   function_name = var.lambda_function_name
-#   handler       = var.handler
-#   runtime       = var.runtime
-#   role          = aws_iam_role.aws_lambda_role.arn
-#   filename      = "${path.module}/lambda_function.zip"
-#   tracing_config {
-#     mode = "Active"
-#   }
+resource "aws_lambda_function" "s3_trigger_lambda" {
+  function_name = var.lambda_function_name
+  handler       = var.handler
+  runtime       = var.runtime
+  role          = aws_iam_role.aws_lambda_role.arn
+  filename      = "${path.module}/lambda_function.zip"
+  tracing_config {
+    mode = "Active"
+  }
 
-#   # Attach the custom log group ARN
-#   environment {
-#     variables = {
-#       CW_LOG_GROUP = aws_cloudwatch_log_group.app_log_group.arn
-#     }
-#   }
-# }
+  # Attach the custom log group ARN
+  environment {
+    variables = {
+      CW_LOG_GROUP = aws_cloudwatch_log_group.app_log_group.arn
+    }
+  }
+}
 
-# #Resource to configure Lambda as an event trigger for S3 bucket.
-# resource "aws_s3_bucket_notification" "s3_event_trigger" {
-#   bucket = aws_s3_bucket.s3_bucket.id
+#Resource to configure Lambda as an event trigger for S3 bucket.
+resource "aws_s3_bucket_notification" "s3_event_trigger" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
-#   lambda_function {
-#     lambda_function_arn = aws_lambda_function.s3_trigger_lambda.arn
-#     events              = var.trigger_events
-#     filter_prefix       = var.filter_prefix
-#     filter_suffix       = var.filter_suffix
-#   }
-# }
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_trigger_lambda.arn
+    events              = var.trigger_events
+    filter_prefix       = var.filter_prefix
+    filter_suffix       = var.filter_suffix
+  }
+}
 
 
-# # To Package lambda function code
-# data "archive_file" "zip_python_code" {
-#   type        = "zip"
-#   source_dir = "${path.module}/../src"
-#   output_path = "${path.module}/lambda_function.zip"
-# }
+# To Package lambda function code
+data "archive_file" "zip_python_code" {
+  type        = "zip"
+  source_dir = "${path.module}/../src"
+  output_path = "${path.module}/lambda_function.zip"
+}
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
-# output "s3_bucket_id" {
-#   description = "The ID of s3 bucket."
-#   value       = aws_s3_bucket.s3_bucket.id
-# }
+output "s3_bucket_id" {
+  description = "The ID of s3 bucket."
+  value       = aws_s3_bucket.s3_bucket.id
+}
 
-# output "s3_bucket_arn" {
-#   description = "The arn of s3 bucket."
-#   value       = aws_s3_bucket.s3_bucket.arn
-# }
+output "s3_bucket_arn" {
+  description = "The arn of s3 bucket."
+  value       = aws_s3_bucket.s3_bucket.arn
+}
 
-# output "lambda_role_arn" {
-#   description = "The arn of lambda role"
-#   value       = aws_iam_role.aws_lambda_role.arn
-# }
+output "lambda_role_arn" {
+  description = "The arn of lambda role"
+  value       = aws_iam_role.aws_lambda_role.arn
+}
