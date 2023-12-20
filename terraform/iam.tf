0533dcb001f995f95ae3910706440d2f0668f8e5
suprajaa-27 <suprajaa96@gmail.com>
2023-12-20 10:28:59 +0100

    updated iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,6 +20,7 @@ resource "aws_iam_role" "aws_lambda_role" {
 resource "aws_iam_policy" "cloud_watch_iam_policy" {
 
   name         = "aws_iam_policy_for_cloud_watch"
+  path         = "/"
   description  = "AWS IAM Policy for cloud watch"
   policy = <<EOF
 {
@@ -57,13 +58,16 @@ resource "aws_iam_policy" "s3_access_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
+      "Effect": "Allow",
       "Action": [
         "s3:GetObject",
         "s3:ListBucket",
         "s3:PutObject"
       ],
-      "Resource": "${aws_s3_bucket.s3_bucket.arn}",
-      "Effect": "Allow"
+      "Resource": [
+        "${aws_s3_bucket.s3_bucket.arn}",
+        "${aws_s3_bucket.s3_bucket.arn}/*"
+      ]
     }
   ]
 }
@@ -80,7 +84,7 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_policy_attachment" {
 resource "aws_lambda_permission" "s3_trigger_permission" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_trigger_lambda.function_name
+  function_name = aws_lambda_function.s3_trigger_lambda_function.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = "${aws_s3_bucket.s3_bucket.arn}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Resource to create a lambda function
 
-resource "aws_lambda_function" "s3_trigger_lambda" {
+resource "aws_lambda_function" "s3_trigger_lambda_function" {
   function_name = var.lambda_function_name
   handler       = var.handler
   runtime       = var.runtime
@@ -17,24 +17,22 @@ resource "aws_lambda_function" "s3_trigger_lambda" {
     }
   }
 }
-
 #Resource to configure Lambda as an event trigger for S3 bucket.
-resource "aws_s3_bucket_notification" "s3_event_trigger" {
+resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.s3_bucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.s3_trigger_lambda.arn
+    lambda_function_arn = aws_lambda_function.s3_trigger_lambda_function.arn
     events              = var.trigger_events
     filter_prefix       = var.filter_prefix
     filter_suffix       = var.filter_suffix
   }
+   depends_on = [aws_lambda_permission.s3_trigger_permission]
 }
-
 
 # To Package lambda function code
 data "archive_file" "zip_python_code" {
   type        = "zip"
-  source_dir = "${path.module}/../src"
-  output_path = "${path.module}/lambda_function.zip"
+  source_dir = "${path.module}"/"${var.lambda_source_dir}"
+  output_path = "${path.module}"/"${var.lambda_output_path}"
 }
-
